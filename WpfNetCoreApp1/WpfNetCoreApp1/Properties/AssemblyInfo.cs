e94ec66ee54f73e7ffb5c2f682c4bada8accfdb4
sfukuoka <sfukuoka@mnes.org>
2020-10-06 10:15:34 +0900

    update code

--- WpfNetCoreApp1/WpfNetCoreApp1/Properties/AssemblyInfo.cs
+++ WpfNetCoreApp1/WpfNetCoreApp1/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WpfNetCoreApp1")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
